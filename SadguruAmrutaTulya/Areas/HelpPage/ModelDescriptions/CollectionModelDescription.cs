84cfc78756485b09d03b58b92a696277bdc57d62
Bolu Asutosh <bolua@cybage.com>
2020-07-09 15:39:36 +0530

    Add project files.

---
+++ SadguruAmrutaTulya/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace SadguruAmrutaTulya.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ SadguruAmrutaTulya/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SadguruAmrutaTulya.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
